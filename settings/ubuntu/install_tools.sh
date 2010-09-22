1522cd85fd678915cdbe22f4d533cfd963e49d76
rx.wen218 <rx.wen218@90eaa59e-d283-11de-a69c-71e0e02f92bf>
2010-09-22 13:11:22 +0000

    fix bugs in install_tools.sh
    git-svn-id: https://rxwen-blog-stuff.googlecode.com/svn/trunk@83 90eaa59e-d283-11de-a69c-71e0e02f92bf

--- settings/ubuntu/install_tools.sh
+++ settings/ubuntu/install_tools.sh
@@ -3,13 +3,13 @@
 # run this script as root, sudo su -l
 
 # sun-java6-jdk must be the last one in array
-array=(wireshark vim-gnome expect sun-java6-jdk)
+array=(wireshark vim-gnome ctags cscope expect ant sun-java6-jdk)
 
 len=${#array[*]}
 i=0
 while [ $i -lt $len ]; do
         echo "sudo apt-get install -y ${array[$i]}"
-        sudo apt-get install -y "$i: ${array[$i]}"
+        sudo apt-get install -y ${array[$i]}
             let i++
         done
 
