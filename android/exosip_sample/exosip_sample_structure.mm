9edbe68fa1be7314010dbcc6d53e4fe0dd59f854
rx.wen218@gmail.com <rx.wen218@gmail.com@90eaa59e-d283-11de-a69c-71e0e02f92bf>
2010-12-14 03:37:08 +0000

    update mind map for exosip_sample
    git-svn-id: https://rxwen-blog-stuff.googlecode.com/svn/trunk@144 90eaa59e-d283-11de-a69c-71e0e02f92bf

--- android/exosip_sample/exosip_sample_structure.mm
+++ android/exosip_sample/exosip_sample_structure.mm
@@ -1,23 +1,23 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1292041107291" ID="ID_1893432311" MODIFIED="1292043183415" TEXT="exosip_sample_root">
+<node CREATED="1292041107291" ID="ID_1893432311" LINK="http://code.google.com/p/rxwen-blog-stuff/source/browse/trunk/android/exosip_sample/" MODIFIED="1292297752603" TEXT="exosip_sample_root">
 <node CREATED="1292041122025" ID="ID_1279409212" MODIFIED="1292041126059" POSITION="right" TEXT="libosip">
-<node CREATED="1292041155045" ID="ID_1702989945" MODIFIED="1292041161111" TEXT="Android.mk"/>
-<node CREATED="1292041163975" ID="ID_287698045" MODIFIED="1292041174376" TEXT="source files"/>
+<node CREATED="1292041155045" ID="ID_1702989945" LINK="http://code.google.com/p/rxwen-blog-stuff/source/browse/trunk/android/exosip_sample/libosip/Android.mk" MODIFIED="1292297706405" TEXT="Android.mk"/>
+<node CREATED="1292041163975" ID="ID_287698045" LINK="http://ftp.gnu.org/gnu/osip/" MODIFIED="1292296056637" TEXT="source files"/>
 </node>
 <node CREATED="1292041128500" ID="ID_1645705385" MODIFIED="1292041131548" POSITION="right" TEXT="libexosip">
-<node CREATED="1292041178332" ID="ID_1381020260" MODIFIED="1292041181273" TEXT="Android.mk"/>
-<node CREATED="1292041181915" ID="ID_367085556" MODIFIED="1292041184401" TEXT="source files"/>
+<node CREATED="1292041178332" ID="ID_1381020260" LINK="http://code.google.com/p/rxwen-blog-stuff/source/browse/trunk/android/exosip_sample/libexosip/Android.mk" MODIFIED="1292297690422" TEXT="Android.mk"/>
+<node CREATED="1292041181915" ID="ID_367085556" LINK="http://download.savannah.gnu.org/releases/exosip/" MODIFIED="1292297675769" TEXT="source files"/>
 </node>
 <node CREATED="1292041132617" ID="ID_985617297" MODIFIED="1292041137595" POSITION="right" TEXT="sip_exe">
 <node CREATED="1292041195512" ID="ID_792063845" MODIFIED="1292041198280" TEXT="jni">
-<node CREATED="1292041211221" ID="ID_1169553565" MODIFIED="1292041216209" TEXT="Android.mk"/>
+<node CREATED="1292041211221" ID="ID_1169553565" LINK="http://code.google.com/p/rxwen-blog-stuff/source/browse/trunk/android/exosip_sample/sip_exe/jni/Android.mk" MODIFIED="1292297732915" TEXT="Android.mk"/>
 <node CREATED="1292041206050" ID="ID_795860918" MODIFIED="1292041209400" TEXT="source files"/>
 </node>
 </node>
 <node CREATED="1292041138157" ID="ID_1758792708" MODIFIED="1292041141121" POSITION="right" TEXT="sip_jni">
 <node CREATED="1292041226723" ID="ID_954817132" MODIFIED="1292041228833" TEXT="jni">
-<node CREATED="1292041267044" ID="ID_1151504895" MODIFIED="1292041269449" TEXT="Android.mk"/>
+<node CREATED="1292041267044" ID="ID_1151504895" LINK="http://code.google.com/p/rxwen-blog-stuff/source/browse/trunk/android/exosip_sample/sip_jni/jni/Android.mk" MODIFIED="1292297740725" TEXT="Android.mk"/>
 <node CREATED="1292041270595" ID="ID_1506053614" MODIFIED="1292041273415" TEXT="source files"/>
 </node>
 <node CREATED="1292041229968" ID="ID_887268623" MODIFIED="1292041232771" TEXT="libs">
@@ -29,6 +29,7 @@
 <node CREATED="1292041299862" ID="ID_1856274464" MODIFIED="1292041301144" TEXT="src">
 <node CREATED="1292041303021" ID="ID_110446201" MODIFIED="1292041307805" TEXT="java source files"/>
 </node>
+<node CREATED="1292297774968" ID="ID_1240056231" LINK="http://code.google.com/p/rxwen-blog-stuff/source/browse/trunk/android/exosip_sample/sip_jni/AndroidManifest.xml" MODIFIED="1292297793719" TEXT="AndroidManifest.xml"/>
 </node>
 </node>
 </map>
