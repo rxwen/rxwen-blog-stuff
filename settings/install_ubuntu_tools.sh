23a0e27e998d5928347740d923b5fcb027f85cd4
rx.wen218@gmail.com <rx.wen218@gmail.com@90eaa59e-d283-11de-a69c-71e0e02f92bf>
2010-12-20 07:51:11 +0000

    add gitk to ubuntu tools
    git-svn-id: https://rxwen-blog-stuff.googlecode.com/svn/trunk@148 90eaa59e-d283-11de-a69c-71e0e02f92bf

--- settings/install_ubuntu_tools.sh
+++ settings/install_ubuntu_tools.sh
@@ -3,7 +3,7 @@
 # run this script as root, sudo su -l
 
 # sun-java6-jdk must be the last one in array
-array=(p7zip tofrodos check g++ cppcheck subversion mercurial git-core valgrind cgdb ipython python-setuptools mit-scheme wireshark nmap vim-gnome ctags cscope expect ant sun-java6-jdk)
+array=(p7zip tofrodos check g++ cppcheck subversion mercurial git-core gitk valgrind cgdb ipython python-setuptools mit-scheme wireshark nmap vim-gnome ctags cscope expect ant sun-java6-jdk)
 
 len=${#array[*]}
 i=0
