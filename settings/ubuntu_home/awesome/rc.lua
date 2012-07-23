daec9be8ea8c2dbb0f80b4587dd41accc33800b9
rx.wen218@gmail.com <rx.wen218@gmail.com@90eaa59e-d283-11de-a69c-71e0e02f92bf>
2012-07-23 13:00:25 +0000

    use runonce to start gnome-settings-daemon to make sure it's started only once
    - liblua5.1-filesystem0 package is required to run 
    
    
    git-svn-id: https://rxwen-blog-stuff.googlecode.com/svn/trunk@334 90eaa59e-d283-11de-a69c-71e0e02f92bf

--- settings/ubuntu_home/awesome/rc.lua
+++ settings/ubuntu_home/awesome/rc.lua
@@ -19,8 +19,6 @@ if awesome.startup_errors then
                      text = awesome.startup_errors })
 end
 
-awful.util.spawn("gnome-settings-daemon")
-
 -- Handle runtime errors after startup
 do
     local in_error = false
@@ -417,3 +415,50 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+
+
+
+-- install liblua5.1-filesystem0 on ubuntu to enable lfs
+require("lfs") 
+-- {{{ Run programm once
+local function processwalker()
+   local function yieldprocess()
+      for dir in lfs.dir("/proc") do
+        -- All directories in /proc containing a number, represent a process
+        if tonumber(dir) ~= nil then
+          local f, err = io.open("/proc/"..dir.."/cmdline")
+          if f then
+            local cmdline = f:read("*all")
+            f:close()
+            if cmdline ~= "" then
+              coroutine.yield(cmdline)
+            end
+          end
+        end
+      end
+    end
+    return coroutine.wrap(yieldprocess)
+end
+
+local function run_once(process, cmd)
+   assert(type(process) == "string")
+   local regex_killer = {
+      ["+"]  = "%+", ["-"] = "%-",
+      ["*"]  = "%*", ["?"]  = "%?" }
+
+   for p in processwalker() do
+      if p:find(process:gsub("[-+?*]", regex_killer)) then
+     return
+      end
+   end
+   return awful.util.spawn(cmd or process)
+end
+-- }}}
+
+---- Usage Example
+run_once("gnome-settings-daemon")
+---- -- Use the second argument, if the programm you wanna start, 
+---- -- differs from the what you want to search.
+---- run_once("redshift", "nice -n19 redshift -l 51:14 -t 5700:4500")
+
