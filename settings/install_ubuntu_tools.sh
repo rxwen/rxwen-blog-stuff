4e55d6b0640dcf38166dd3683cee02f7ef5da88d
rx.wen218@gmail.com <rx.wen218@gmail.com@90eaa59e-d283-11de-a69c-71e0e02f92bf>
2013-01-09 13:00:08 +0000

    add clang to ubuntu tools installation
    git-svn-id: https://rxwen-blog-stuff.googlecode.com/svn/trunk@373 90eaa59e-d283-11de-a69c-71e0e02f92bf

--- settings/install_ubuntu_tools.sh
+++ settings/install_ubuntu_tools.sh
@@ -3,7 +3,7 @@
 # run this script as root, sudo su -l
 
 # sun-java6-jdk must be the last one in array
-array=(sysv-rc-conf ncurses-dev p7zip tofrodos check g++ cppcheck pylint mercurial git-core gitk valgrind cgdb ipython python-pip mit-scheme wireshark nmap vim ctags cscope expect flex doxygen graphviz sdcv dmenu ant manpages-posix-dev)
+array=(sysv-rc-conf ncurses-dev p7zip tofrodos check g++ cppcheck pylint mercurial git-core gitk valgrind cgdb ipython python-pip mit-scheme wireshark nmap vim ctags cscope expect flex doxygen graphviz sdcv dmenu ant manpages-posix-dev clang)
 
 len=${#array[*]}
 i=0
