dd6cc36f4d17fb90d339ca3fd2dd2486e99ecc43
rx.wen218@gmail.com <rx.wen218@gmail.com@90eaa59e-d283-11de-a69c-71e0e02f92bf>
2013-01-11 13:04:09 +0000

    remove mit-scheme from ubuntu tools installation
    git-svn-id: https://rxwen-blog-stuff.googlecode.com/svn/trunk@375 90eaa59e-d283-11de-a69c-71e0e02f92bf

--- settings/install_ubuntu_tools.sh
+++ settings/install_ubuntu_tools.sh
@@ -3,7 +3,7 @@
 # run this script as root, sudo su -l
 
 # sun-java6-jdk must be the last one in array
-array=(sysv-rc-conf ncurses-dev p7zip tofrodos check g++ cppcheck pylint mercurial git-core gitk valgrind cgdb ipython python-pip mit-scheme wireshark nmap vim ctags cscope expect flex doxygen graphviz sdcv dmenu ant manpages-posix-dev clang libclang-dev)
+array=(sysv-rc-conf ncurses-dev p7zip tofrodos check g++ cppcheck pylint mercurial git-core gitk valgrind cgdb ipython python-pip wireshark nmap vim ctags cscope expect flex doxygen graphviz sdcv dmenu ant manpages-posix-dev clang libclang-dev)
 
 len=${#array[*]}
 i=0
