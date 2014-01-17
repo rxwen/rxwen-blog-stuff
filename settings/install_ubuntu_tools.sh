9f866029a81250ce93bde8b33e655669f7d2e550
rx.wen218@gmail.com <rx.wen218@gmail.com@90eaa59e-d283-11de-a69c-71e0e02f92bf>
2014-01-17 15:15:51 +0000

    add astyle(for code formatting)
    git-svn-id: https://rxwen-blog-stuff.googlecode.com/svn/trunk@408 90eaa59e-d283-11de-a69c-71e0e02f92bf

--- settings/install_ubuntu_tools.sh
+++ settings/install_ubuntu_tools.sh
@@ -3,7 +3,7 @@
 # run this script as root, sudo su -l
 
 # sun-java6-jdk must be the last one in array
-array=(udisks-glue zsh autojump sysv-rc-conf ncurses-dev p7zip tofrodos check g++ cppcheck pylint mercurial git-core gitk git-svn valgrind cgdb ipython python-pip wireshark nmap vim ctags cscope expect flex doxygen graphviz sdcv dmenu ant manpages-posix-dev clang libclang-dev)
+array=(udisks-glue zsh autojump sysv-rc-conf ncurses-dev p7zip tofrodos check g++ cppcheck pylint mercurial git-core gitk git-svn valgrind cgdb ipython python-pip wireshark nmap vim ctags cscope expect flex doxygen graphviz sdcv dmenu ant manpages-posix-dev clang libclang-dev astyle)
 
 len=${#array[*]}
 i=0
