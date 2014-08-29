c743da25b5b4a8abfd015d74cc3d643990e3d141
rx.wen218@gmail.com <rx.wen218@gmail.com@90eaa59e-d283-11de-a69c-71e0e02f92bf>
2014-08-29 14:48:23 +0000

    create a bat script to setup windows environment quickly
    - use setx command to set user PATH environment permanently
    - use setx /m command to set system PATH environment permanently
    - use xcopy to copy directory to correct place
    
    git-svn-id: https://rxwen-blog-stuff.googlecode.com/svn/trunk@438 90eaa59e-d283-11de-a69c-71e0e02f92bf

---
+++ settings/setup_windows_env.bat
@@ -0,0 +1,4 @@
+setx PATH "C:\tools\cygwin64\bin;%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;" /m
+setx PATH "C:\tools\apps"
+md c:\tools\
+xcopy C:\src\windows_tools\vim c:\tools\vim\ /O /E /Y
