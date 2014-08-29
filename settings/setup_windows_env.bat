2ac660b3916b7fb6ec4b8dc5fad4d242a6740b94
rx.wen218@gmail.com <rx.wen218@gmail.com@90eaa59e-d283-11de-a69c-71e0e02f92bf>
2014-08-29 15:15:21 +0000

    update setup_windows_env.bat to import reg file to registry
    git-svn-id: https://rxwen-blog-stuff.googlecode.com/svn/trunk@439 90eaa59e-d283-11de-a69c-71e0e02f92bf

--- settings/setup_windows_env.bat
+++ settings/setup_windows_env.bat
@@ -2,3 +2,4 @@ setx PATH "C:\tools\cygwin64\bin;%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.
 setx PATH "C:\tools\apps"
 md c:\tools\
 xcopy C:\src\windows_tools\vim c:\tools\vim\ /O /E /Y
+reg import windbg_context_menu.reg
