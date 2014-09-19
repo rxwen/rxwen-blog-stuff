972fa15db2925422cf77a4e079792bb960568f9b
rx.wen218@gmail.com <rx.wen218@gmail.com@90eaa59e-d283-11de-a69c-71e0e02f92bf>
2014-09-19 14:30:24 +0000

    update setup_windows_env.bat according to tools I needed
    git-svn-id: https://rxwen-blog-stuff.googlecode.com/svn/trunk@445 90eaa59e-d283-11de-a69c-71e0e02f92bf

--- settings/setup_windows_env.bat
+++ settings/setup_windows_env.bat
@@ -1,5 +1,8 @@
-setx PATH "C:\tools\cygwin64\bin;%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;" /m
-setx PATH "C:\tools\apps"
-md c:\tools\
-xcopy C:\src\windows_tools\vim c:\tools\vim\ /O /E /Y
-reg import windbg_context_menu.reg
+set TOOLS_PATH=C:\tools
+set GOROOT_VARIABLE=%TOOLS_PATH%\go
+REM setx PATH "%TOOLS_PATH%\cygwin64\bin;%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;" /m
+setx GOROOT=%GOROOT_VARIABLE%
+setx PATH "%TOOLS_PATH%\apps;%TOOLS_PATH%\Python27;%TOOLS_PATH%\Debuggers\x64;%TOOLS_PATH%\nmap;%TOOLS_PATH%\GnuPG;%TOOLS_PATH%\graphviz\bin;%TOOLS_PATH%\java_sdk\bin;%TOOLS_PATH%\LLVM\bin;%TOOLS_PATH%\pandoc;%TOOLS_PATH%\Sysinternals;%TOOLS_PATH%\;%TOOLS_PATH%\texlive\bin\win32;%GOROOT_VARIABLE%\bin;"
+REM md %TOOLS_PATH%
+REM xcopy C:\src\windows_tools\vim c:\tools\vim\ /O /E /Y
+REM reg import windbg_context_menu.reg
