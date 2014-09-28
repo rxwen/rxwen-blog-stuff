9cf6ee1f71c782625f8e7981be09110a8eb3ada4
rx.wen218@gmail.com <rx.wen218@gmail.com@90eaa59e-d283-11de-a69c-71e0e02f92bf>
2014-09-28 14:24:10 +0000

    update script for setting up windows environment
    git-svn-id: https://rxwen-blog-stuff.googlecode.com/svn/trunk@448 90eaa59e-d283-11de-a69c-71e0e02f92bf

--- settings/setup_windows_env.bat
+++ settings/setup_windows_env.bat
@@ -1,8 +1,11 @@
 set TOOLS_PATH=C:\tools
 set GOROOT_VARIABLE=%TOOLS_PATH%\go
-REM setx PATH "%TOOLS_PATH%\cygwin64\bin;%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;" /m
-setx GOROOT=%GOROOT_VARIABLE%
-setx PATH "%TOOLS_PATH%\apps;%TOOLS_PATH%\Python27;%TOOLS_PATH%\Debuggers\x64;%TOOLS_PATH%\nmap;%TOOLS_PATH%\GnuPG;%TOOLS_PATH%\graphviz\bin;%TOOLS_PATH%\java_sdk\bin;%TOOLS_PATH%\LLVM\bin;%TOOLS_PATH%\pandoc;%TOOLS_PATH%\Sysinternals;%TOOLS_PATH%\;%TOOLS_PATH%\texlive\bin\win32;%GOROOT_VARIABLE%\bin;"
+set GOPATH_VARIABLE=%USERPROFILE%\projects\go
+REM setx PATH "%TOOLS_PATH%\vim\vim74;%TOOLS_PATH%\cygwin64\bin;%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;" /M
+setx GOPATH %GOPATH_VARIABLE%
+setx GOBIN %GOPATH_VARIABLE%\bin
+setx GOROOT %GOROOT_VARIABLE%
+setx PATH "%TOOLS_PATH%\apps;%TOOLS_PATH%\Python27;%TOOLS_PATH%\Debuggers\x64;%TOOLS_PATH%\nmap;%TOOLS_PATH%\GnuPG;%TOOLS_PATH%\graphviz\bin;%TOOLS_PATH%\java_sdk\bin;%TOOLS_PATH%\LLVM\bin;%TOOLS_PATH%\wireshark;%TOOLS_PATH%\pandoc;%TOOLS_PATH%\Sysinternals;%TOOLS_PATH%\Mercurial;%TOOLS_PATH%\texlive\bin\win32;%GOROOT_VARIABLE%\bin;%GOBIN%"
 REM md %TOOLS_PATH%
 REM xcopy C:\src\windows_tools\vim c:\tools\vim\ /O /E /Y
 REM reg import windbg_context_menu.reg
