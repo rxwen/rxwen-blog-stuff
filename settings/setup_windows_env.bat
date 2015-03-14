925e3fcd078ab49a5396af96490f41ff1ad25c34
rxwen <rx.wen218@gmail.com>
2015-03-14 17:30:48 +0800

    update setup windows env script

--- settings/setup_windows_env.bat
+++ settings/setup_windows_env.bat
@@ -5,7 +5,7 @@ REM setx PATH "%TOOLS_PATH%\vim\vim74;%TOOLS_PATH%\cygwin64\bin;%PATH%;C:\Window
 setx GOPATH %GOPATH_VARIABLE%
 setx GOBIN %GOPATH_VARIABLE%\bin
 setx GOROOT %GOROOT_VARIABLE%
-setx PATH "%TOOLS_PATH%\apps;%TOOLS_PATH%\Python27;%TOOLS_PATH%\Python27\scripts;%TOOLS_PATH%\Debuggers\x64;%TOOLS_PATH%\nmap;%TOOLS_PATH%\GnuPG;%TOOLS_PATH%\graphviz\bin;%TOOLS_PATH%\java_sdk\bin;%TOOLS_PATH%\LLVM\bin;%TOOLS_PATH%\wireshark;%TOOLS_PATH%\pandoc;%TOOLS_PATH%\Sysinternals;%TOOLS_PATH%\Mercurial;%TOOLS_PATH%\gradle\bin;%TOOLS_PATH%\texlive\bin\win32;%TOOLS_PATH%\ruby\bin;%GOROOT_VARIABLE%\bin;%GOBIN%";%TOOLS_PATH%\android_sdk\tools;%TOOLS_PATH%\android_sdk\platform-tools;%TOOLS_PATH%\Git\cmd;%TOOLS_PATH%\cmake\bin\;%TOOLS_PATH%\doxygen\bin;%TOOLS_PATH%\ConEmu\;
+setx PATH "%TOOLS_PATH%\apps;%TOOLS_PATH%\Python27;%TOOLS_PATH%\Python27\scripts;%TOOLS_PATH%\Debuggers\x64;%TOOLS_PATH%\nmap;%TOOLS_PATH%\GnuPG;%TOOLS_PATH%\graphviz\bin;%TOOLS_PATH%\java_sdk\bin;%TOOLS_PATH%\LLVM\bin;%TOOLS_PATH%\wireshark;%TOOLS_PATH%\pandoc;%TOOLS_PATH%\Sysinternals;%TOOLS_PATH%\Mercurial;%TOOLS_PATH%\gradle\bin;%TOOLS_PATH%\texlive\bin\win32;%TOOLS_PATH%\ruby\bin;%GOROOT_VARIABLE%\bin;%GOBIN%";%TOOLS_PATH%\android_sdk\tools;%TOOLS_PATH%\android_sdk\platform-tools;%TOOLS_PATH%\Git\cmd;%TOOLS_PATH%\cmake\bin\;%TOOLS_PATH%\doxygen\bin;
 REM md %TOOLS_PATH%
 REM xcopy C:\src\windows_tools\vim c:\tools\vim\ /O /E /Y
 REM reg import windbg_context_menu.reg
