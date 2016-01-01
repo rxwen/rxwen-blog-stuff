7c55c8891fb9ceb8c9ed693dea4a8e45dcd4e3d5
rxwen <rx.wen218@gmail.com>
2016-01-02 00:30:01 +0800

    update fzf.sh

--- settings/ubuntu_home/.fzf.sh
+++ settings/ubuntu_home/.fzf.sh
@@ -21,17 +21,12 @@ fgit-log() {
   done
 }
 
-# Modified version where you can press
-#   - CTRL-O to open with `open` command,
-#   - CTRL-E or Enter key to open with the $EDITOR
-fo() {
-    local out file key
-    out=$(fzf --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)
-    key=$(head -1 <<< "$out")
-    file=$(head -2 <<< "$out" | tail -1)
-    if [ -n "$file" ]; then
-        [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
-    fi
+# fgit-reflog - checkout git reflog
+fgit-reflog() {
+    local reflogs sha
+    reflogs=$(git reflog) &&
+        sha=$(echo "$reflogs" | fzf --tac +s +m -e) &&
+        git checkout $(echo "$sha" | sed "s/ .*//")
 }
 
 # fgit-branch - checkout git branch (including remote branches)
@@ -52,6 +47,60 @@ fgit-tag() {
     (echo "$tags"; echo "$branches") |
     fzf --no-hscroll --ansi +m -d "\t" -n 2) || return
     git checkout $(echo "$target" | awk '{print $2}')
+}
+
+# fgit-show - git commit browser
+fgit-show() {
+    git log --graph --color=always \
+        --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+    fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+        --bind "ctrl-m:execute:
+    (grep -o '[a-f0-9]\{7\}' | head -1 |
+    xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+    {}
+    FZF-EOF"
+}
+
+# fgit-stash - easier way to deal with stashes
+# type fstash to get a list of your stashes
+# enter shows you the contents of the stash
+# ctrl-d shows a diff of the stash against your current HEAD
+# ctrl-b checks the stash out as a branch, for easier merging
+fgit-stash() {
+    local out q k sha
+    while out=$(
+        git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
+        fzf --ansi --no-sort --query="$q" --print-query \
+            --expect=ctrl-d,ctrl-b);
+    do
+        mapfile -t out <<< "$out"
+        q="${out[0]}"
+        k="${out[1]}"
+        sha="${out[-1]}"
+        sha="${sha%% *}"
+        [[ -z "$sha" ]] && continue
+        if [[ "$k" == 'ctrl-d' ]]; then
+            git diff $sha
+        elif [[ "$k" == 'ctrl-b' ]]; then
+            git stash branch "stash-$sha" $sha
+            break;
+        else
+            git stash show -p $sha
+        fi
+    done
+}
+
+# Modified version where you can press
+#   - CTRL-O to open with `open` command,
+#   - CTRL-E or Enter key to open with the $EDITOR
+fo() {
+    local out file key
+    out=$(fzf --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)
+    key=$(head -1 <<< "$out")
+    file=$(head -2 <<< "$out" | tail -1)
+    if [ -n "$file" ]; then
+        [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
+    fi
 }
 
 # fs [FUZZY PATTERN] - Select selected tmux session
@@ -83,7 +132,3 @@ ftpane () {
     fi
 }
 
-#z() {
-    #local dir
-    #dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
-#}
