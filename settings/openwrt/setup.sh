5754bb81667905ace748fc20286a11d9e0ced833
Raymond Wen <rx.wen218@gmail.com>
2016-02-24 10:24:04 +0800

    improve openwrt setup.sh

--- settings/openwrt/setup.sh
+++ settings/openwrt/setup.sh
@@ -1,3 +1,4 @@
+# use http://mirrors.ustc.edu.cn/openwrt/ instead of http://downloads.openwrt.org in opkg cfg (/etc/opkg/) to improve speed
 opkg update
 # for usb devices 
 opkg install kmod-usb-storage 
@@ -8,7 +9,10 @@ opkg install block-mount kmod-fs-ext4
 # for ntfs file system
 opkg install ntfs-3g samba luci-app-samba
 
+opkg install iptables-mod-nat-extra
+
 opkg install pdnsd
+# or consider using dnscrypt-proxy, edit /etc/config/dnscrypt-proxy after installation
 
 opkg install libpolarssl
 # download shadowsocks from: https://github.com/shadowsocks/openwrt-shadowsocks
