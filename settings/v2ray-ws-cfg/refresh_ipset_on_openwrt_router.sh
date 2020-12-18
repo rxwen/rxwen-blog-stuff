b2cae7a07649a95abdc6fc672c0bbefe0d8ca232
Ruixiong Wen <rx.wen218@gmail.com>
2020-12-18 10:27:39 +0800

    add refresh ipset on openwrt

---
+++ settings/v2ray-ws-cfg/refresh_ipset_on_openwrt_router.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /root/chnroute.txt
+#curl http://www.ipdeny.com/ipblocks/data/countries/cn.zone > /root/chnroute.txt
+#echo "add apple ip range"
+## https://support.apple.com/en-us/HT210060
+#echo "17.0.0.0/8" >> /root/chnroute.txt
+
+ipset destroy chnroute
+#ipset -N chnroute hash:net maxelem 65536
+
+for ip in $(cat '/root/chnroute.txt'); do
+    ipset add chnroute $ip
+done
+
+/etc/init.d/firewall restart
