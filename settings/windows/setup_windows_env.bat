e762d4bef9003e85684768b20c48d889270e0041
rxwen <rx.wen218@gmail.com>
2023-09-21 08:21:03 +0800

    update setup_windows_env to set user PATH environment variable permanently

--- settings/windows/setup_windows_env.bat
+++ settings/windows/setup_windows_env.bat
@@ -1,14 +1,39 @@
-set TOOLS_PATH=C:\tools
+set TOOLS_PATH=D:\tools
+set PROJECT_PATH=D:\projects
 set GOROOT_VARIABLE=%TOOLS_PATH%\go
-set GOPATH_VARIABLE=%USERPROFILE%\projects\go
-@REM setx PATH "%TOOLS_PATH%\vim\vim74;%TOOLS_PATH%\cygwin64\bin;%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;" /M
-setx GOPATH %GOPATH_VARIABLE%
-setx GOBIN %GOPATH_VARIABLE%\bin
-setx GOROOT %GOROOT_VARIABLE%
-setx JAVA_HOME %TOOLS_PATH%\java_sdk\jdk1.8
-setx JAVA7_HOME %TOOLS_PATH%\java_sdk\jdk1.7
-setx JAVA8_HOME %TOOLS_PATH%\java_sdk\jdk1.8
-setx PATH "%TOOLS_PATH%\apps;%TOOLS_PATH%\Python27;%TOOLS_PATH%\Python27\scripts;%TOOLS_PATH%\Debuggers\x64;%TOOLS_PATH%\nmap;%TOOLS_PATH%\GnuPG;%TOOLS_PATH%\graphviz\bin;%JAVA_HOME%\bin;%TOOLS_PATH%\LLVM\bin;%TOOLS_PATH%\wireshark;%TOOLS_PATH%\pandoc;%TOOLS_PATH%\Sysinternals;%TOOLS_PATH%\Mercurial;%TOOLS_PATH%\gradle\bin;%TOOLS_PATH%\texlive\bin\win32;%TOOLS_PATH%\ruby\bin;%GOROOT_VARIABLE%\bin;%GOBIN%";%TOOLS_PATH%\android_sdk\tools;%TOOLS_PATH%\android_sdk\platform-tools;%TOOLS_PATH%\android_sdk\ndk;%TOOLS_PATH%\Git\cmd;%TOOLS_PATH%\cmake\bin\;%TOOLS_PATH%\doxygen\bin;%TOOLS_PATH%\putty\;%TOOLS_PATH%\ffmpeg\bin\;
+set GOPATH_VARIABLE=%PROJECT_PATH%\go
+REM setx GOPATH %GOPATH_VARIABLE%
+REM setx GOBIN %GOPATH_VARIABLE%\bin
+REM setx GOROOT %GOROOT_VARIABLE%
+REM setx JAVA_HOME %TOOLS_PATH%\jdk-11.0.2
+
+REM set PATHVAL=%PATH%
+set PATHVAL=%TOOLS_PATH%\apps
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\Python27;%TOOLS_PATH%\Python27\scripts
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\windbg\x64
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\nmap
+set PATHVAL=%PATHVAL%;%JAVA_HOME%\bin
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\LLVM\bin
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\wireshark\App\Wireshark
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\sysinternals
+set PATHVAL=%PATHVAL%;%GOROOT_VARIABLE%\bin;%GOBIN%"
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\Android_Sdk\tools;%TOOLS_PATH%\Android_Sdk\platform-tools;%TOOLS_PATH%\Android_Sdk\cmdline-tools\latest\bin;%TOOLS_PATH%\android_sdk\ndk\25.2.9519653
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\Git\cmd
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\Vim\vim90
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\cmake\bin
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\doxygen\bin
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\putty
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\nodejs
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\ditto
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\ffmpeg\bin
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\flutter\bin
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\everything
+set PATHVAL=%PATHVAL%;%USERPROFILE%\AppData\Local\Programs\Python\Python311\Scripts;%USERPROFILE%\AppData\Local\Programs\Python\Python311
+set PATHVAL=%PATHVAL%;%USERPROFILE%\AppData\Local\Microsoft\Windows\Apps
+set PATHVAL=%PATHVAL%;%USERPROFILE%\AppData\Local\Programs\Microsoft VS Code
+set PATHVAL=%PATHVAL%;%USERPROFILE%\AppData\Roaming\nmp
+echo %PATHVAL%
+setx PATH %PATHVAL%
 REM md %TOOLS_PATH%
 REM xcopy C:\src\windows_tools\vim c:\tools\vim\ /O /E /Y
 REM reg import windbg_context_menu.reg
