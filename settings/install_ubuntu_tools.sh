fecf231e670b680fb9ccfc69700041a2861aeada
rxwen <rx.wen218@gmail.com>
2023-10-10 20:20:30 +0800

    update windows setup bat

--- settings/install_ubuntu_tools.sh
+++ settings/install_ubuntu_tools.sh
@@ -2,7 +2,7 @@
 
 # run this script as root, sudo su -l
 
-array=(sysstat iotop dos2unix nethogs iftop apt-file ipset htop dsniff iftop openssh-server awesome awesome-extra gnome-settings-daemon realpath socat proxychains gpick gnupg2 silversearcher-ag shutter parcellite zsh mpg123 ncurses-dev p7zip tofrodos check g++ cppcheck mercurial git-core git-extras gitk tig git-svn valgrind cgdb python-pip wireshark nmap vim-gnome ctags cscope expect flex doxygen graphviz pandoc goldendict sdcv dmenu manpages-posix-dev clang libclang-dev astyle global libc6-dev-i386 autossh glances zoxide)
+array=(sysstat iotop dos2unix nethogs iftop apt-file ipset htop dsniff iftop realpath socat silversearcher-ag zsh ncurses-dev p7zip tofrodos check g++ cppcheck git-core git-extras tig valgrind cgdb nmap ctags cscope expect flex manpages-posix-dev clang libclang-dev astyle)
 
 len=${#array[*]}
 i=0
--- settings/windows/setup_windows_env.bat
+++ settings/windows/setup_windows_env.bat
@@ -6,6 +6,8 @@ REM setx GOPATH %GOPATH_VARIABLE%
 REM setx GOBIN %GOPATH_VARIABLE%\bin
 REM setx GOROOT %GOROOT_VARIABLE%
 setx JAVA_HOME %TOOLS_PATH%\jdk-11.0.2
+set JAVA_HOME=%TOOLS_PATH%\jdk-11.0.2
+setx LANGUAGE en_US
 
 REM set PATHVAL=%PATH%
 set PATHVAL=%TOOLS_PATH%\apps
@@ -15,16 +17,18 @@ set PATHVAL=%PATHVAL%;%JAVA_HOME%\bin
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\LLVM\bin
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\wireshark\App\Wireshark
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\sysinternals
-set PATHVAL=%PATHVAL%;%GOROOT_VARIABLE%\bin;%GOBIN%
+set PATHVAL=%PATHVAL%;%GOROOT_VARIABLE%\bin
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\Android_Sdk\tools;%TOOLS_PATH%\Android_Sdk\platform-tools;%TOOLS_PATH%\Android_Sdk\cmdline-tools\latest\bin;%TOOLS_PATH%\android_sdk\ndk\25.2.9519653
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\Vim\vim90
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\cmake\bin
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\GnuMake\bin
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\doxygen\bin
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\putty
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\nodejs
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\ditto
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\ffmpeg\bin
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\flutter\bin
+set PATHVAL=%PATHVAL%;%TOOLS_PATH%\zbar
 set PATHVAL=%PATHVAL%;%TOOLS_PATH%\everything
 set PATHVAL=%PATHVAL%;%%JAVA_HOME%%\bin
 set PATHVAL=%PATHVAL%;%USERPROFILE%\AppData\Local\Programs\Python\Python311\Scripts;%USERPROFILE%\AppData\Local\Programs\Python\Python311
@@ -33,7 +37,7 @@ set PATHVAL=%PATHVAL%;%USERPROFILE%\AppData\Local\Microsoft\WindowsApps
 set PATHVAL=%PATHVAL%;%USERPROFILE%\AppData\Local\Programs\Microsoft VS Code
 set PATHVAL=%PATHVAL%;%USERPROFILE%\AppData\Roaming\nmp
 echo %PATHVAL%
-setx PATH %PATHVAL%
+setx PATH "%PATHVAL%"
 REM md %TOOLS_PATH%
 REM xcopy C:\src\windows_tools\vim c:\tools\vim\ /O /E /Y
 REM reg import windbg_context_menu.reg
